b4a876c541648845c0c9377c10af66bc4160e6fc
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-01-16 17:37:05 -0500

    full transformation

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-require('./calc')
-// const add = require('./calc')
-// console.log('2 + 3 =', add(2, 3))
+const add = require('./calc')
+console.log('2 + 3 =', add(2, 3))
 // node index.js
 // 2 + 3 = 5
